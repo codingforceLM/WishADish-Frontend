6bf89ad8d5661cd5b14dff21182d8d5865f8150c
mbl <mbl.85@web.de>
2021-03-17 19:54:47 +0100

    Added login page and appbar with nav drawer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,61 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+        color="deep-purple"
+        dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+    >
+      <v-list
+          nav
+          dense
+      >
+        <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <router-view></router-view>
+  </v-app>
 </template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data: () =>({
+    drawer: false,
+    group: null,
+  })
+}
+
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container>
+    <div id="content">
+      <v-row justify="center">
+        <v-col cols="5">
+          <v-img
+              :src="require('../assets/wishdishlogo.png')"
+              contain
+              height="400"
+          />
+          <v-btn class="my-3" block >
+            <v-icon>mdi-facebook</v-icon>
+            <v-spacer></v-spacer>
+            Sign-In with facebook
+          </v-btn>
+          <v-btn class="my-3" block>
+            <v-icon>mdi-apple</v-icon>
+            <v-spacer></v-spacer>
+            Sign-In with apple
+          </v-btn>
+
+          <v-btn class="my-3" block>
+            <v-icon>mdi-google</v-icon>
+            <v-spacer></v-spacer>
+            Sign-In with google
+          </v-btn>
+          <span class="ma-2"></span>
+          <v-text-field
+              class="my-3"
+              label="E-Mail"
+              :rules="rules"
+              hide-details="auto"
+              outlined
+          ></v-text-field>
+          <v-text-field
+              class="my-3"
+              label="Password"
+              :rules="rules"
+              hide-details="auto"
+              :type="'password'"
+              outlined
+          ></v-text-field>
+          <v-btn class="my-3" block>
+            Login
+          </v-btn>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
+
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+
+    data: () => ({
+
+    }),
+  }
+</script>
+
