0d47bda2ca4fbd367eadc43ac36c0d168e91fb1d
mbl <mbl.85@web.de>
2021-03-22 22:26:27 +0100

    Added basic add popup

--- src/components/DishList.vue
+++ src/components/DishList.vue
@@ -2,15 +2,117 @@
   <v-container>
     <v-row wrap align-end>
       <h1>Eigene Gerichte</h1>
-      <v-btn
-          class="mx-2"
-          dark
-          color="indigo"
+      <v-dialog
+          v-model="dialog"
+          max-width="60%"
+          scrollable
       >
-        <v-icon dark>
-          mdi-plus
-        </v-icon>
-      </v-btn>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+              class="mx-2"
+              dark
+              color="indigo"
+              v-bind="attrs"
+              v-on="on"
+          >
+            <v-icon dark>
+              mdi-plus
+            </v-icon>
+          </v-btn>
+
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline font-weight-bold">Gericht erstellen</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col
+                    cols="12"
+                >
+                  <v-text-field
+                      label="Titel"
+                      required
+                  ></v-text-field>
+                </v-col>
+
+                <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                >
+                  <!-- TODO properly align items in card -->
+                  <v-card
+                      width="100%"
+                      v-for="entry in entries" :key="entry.id"
+                      class="my-1"
+                  >
+                    <v-col
+                      cols="12"
+                    >
+                      <v-select
+                          :items="ingredients"
+                          label="Zutat"
+                          v-model="entry.ingredient"
+                      ></v-select>
+                      <v-text-field
+                          label="Menge"
+                          required
+                          v-model="entry.amount"
+                      ></v-text-field>
+                      <v-select
+                          :items="units"
+                          label="Einheit"
+                          v-model="entry.unit"
+                      ></v-select>
+                      <v-btn
+                          class="mt-2"
+                          dark
+                          color="red"
+                          @click="removeEntry(entry)"
+                      >
+                        <v-icon dark>
+                          mdi-close
+                        </v-icon>
+                      </v-btn>
+                    </v-col>
+                  </v-card>
+                  <v-btn
+                      class="mt-2"
+                      dark
+                      color="indigo"
+                      @click="newEntry()"
+                  >
+                    <v-icon dark>
+                      mdi-plus
+                    </v-icon>
+                  </v-btn>
+                </v-col>
+
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                color="blue darken-1"
+                text
+                @click="dialog = false"
+            >
+              Close
+            </v-btn>
+            <v-btn
+                color="blue darken-1"
+                text
+                @click="dialog = false"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
       <div class="break"></div>
       <v-col align="start">
 
@@ -68,11 +170,25 @@ import {mapActions, mapGetters} from "vuex";
 
 export default {
   name: "DishList",
+  data: () => ({
+    dialog: false,
+    ingredients: ["ingrd a", "ingrd b", "ingrd c", "ingrd d", "ingrd e"],
+    units: ["liter", "milliliter", "gramm", "kilogramm"],
+    id: 1,
+    entries: [{id: 1, ingredient: "", amount: "", unit: ""}]
+  }),
   methods: {
     ...mapActions(["fetchDishes", "fetchDish"]),
     changeDetails: function (id) {
       this.fetchDish(id)
       console.log(id)
+    },
+    removeEntry(entry) {
+      let index = this.entries.indexOf(entry);
+      this.entries.splice(index, 1);
+    },
+    newEntry() {
+      this.entries.push({id: this.id, ingredient: "", amount: "", unit: ""})
     }
   },
   computed: mapGetters(["allDishes", "singleDish"]),
