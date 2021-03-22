6eed8388ba324dcf544ef49ea7ad057855b176d3
Batschko <lukas.h2@hotmail.de>
2021-03-22 14:44:14 +0100

    created shoppinglist view

---
+++ src/views/ShoppingList.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app fluid >
+    <v-layout row justify-space-between >
+      <v-col md="6" >
+        <ShoppingListList />
+      </v-col>
+      <v-col md="6">
+        <ShoppingListDetails />
+      </v-col>
+    </v-layout>
+  </v-app>
+</template>
+
+<script>
+
+
+import ShoppingListList from "@/components/ShoppingListList";
+import ShoppingListDetails from "@/components/ShoppingListDetails";
+export default {
+  name: "ShoppingList",
+  components: {ShoppingListDetails, ShoppingListList},
+}
+</script>
+
+<style scoped>
+
+</style>
